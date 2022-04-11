05a863513a5c7929da4074b500166111a1f327da
Nicolas Pearson <nic.s.pearson@gmail.com>
2022-04-11 16:35:51 +0200

    test(ewl): set coverage threshold to 100

--- packages/ewl/test/jest.config.js
+++ packages/ewl/test/jest.config.js
@@ -2,7 +2,7 @@ module.exports = Object.assign({}, require(`kalos/packages/ewl/jest.config.js`),
   collectCoverageFrom: ['src/**/*.ts'],
   coverageThreshold: {
     global: {
-      branches: 92.3,
+      branches: 100,
       functions: 100,
       lines: 100,
       statements: 100,
